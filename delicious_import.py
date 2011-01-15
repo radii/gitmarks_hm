04c28b36842f7fbd71b9c3e5d12760be9983a985
Far McKon <FarMcKon@gmail.com>
2011-01-15 13:38:58 -0500

    Cleanup and formatting and error caching changes. No functinoality changes.
    gitmarks.py
    - Changed '-push' default to be intutive
    - added except for InvalidURL eror on url open
    - converted instance methods to class methods
    - added 'lazyAdd' option that does not re-add existing content.
    - switched path combining to use os.path.join for platform-cros-ability
    
    delicious_import.py
    - added catch for mindom exception errors
    - added check for yahoo error page
    - renamed function for clarity

--- delicious_import.py
+++ delicious_import.py
@@ -10,54 +10,69 @@ Copyright (c) 2010 Hilary Mason. All rights reserved.
 import sys, os
 import urllib
 from xml.dom import minidom
+from xml.parsers import expat
 from optparse import OptionParser
 
 from gitmark import gitMark
 
-class delicious_import(object):
-    def __init__(self, username, password=''):
-		# API URL: https://user:passwd@api.del.icio.us/v1/posts/all
-        url = "https://%s:%s@api.del.icio.us/v1/posts/all" % (username, password)
-        h = urllib.urlopen(url)
-        content = h.read()
-        h.close()
-                
-        x = minidom.parseString(content)
-        
-        # sample post: <post href="http://www.pixelbeat.org/cmdline.html" hash="e3ac1d1e4403d077ee7e65f62a55c406" description="Linux Commands - A practical reference" tag="linux tutorial reference" time="2010-11-29T01:07:35Z" extended="" meta="c79362665abb0303d577b6b9aa341599" />
-        post_list = x.getElementsByTagName('post')
-        for post_index, post in enumerate(post_list):
-            url = post.getAttribute('href')
-            desc = post.getAttribute('description')
-            tags = ",".join([t for t in post.getAttribute('tag').split()])
-            timestamp = post.getAttribute('time')
-            
-            options = {}
-            options['tags'] = tags
-            options['push'] = False
-            options['msg'] = desc
-            
-            # Set the authoring date of the commit based on the imported
-            # timestamp. git reads the GIT_AUTHOR_DATE environment var.
-            os.environ['GIT_AUTHOR_DATE'] = timestamp
+def import_delicious_to_local_git(username, password=''):
+	# API URL: https://user:passwd@api.del.icio.us/v1/posts/all
+	url = "https://%s:%s@api.del.icio.us/v1/posts/all" % (username, password)
+	h = urllib.urlopen(url)
+	content = h.read()
+	h.close()
 
-            args = [url]
-            g = gitMark(options, args)
-            
-            # Reset authoring timestamp (abundance of caution)
-            del os.environ['GIT_AUTHOR_DATE']
-            
-            if post_list.length > 1:
-                print '%d of %d bookmarks imported (%d%%)' % (
-                    post_index + 1, post_list.length,
-                    (post_index + 1.0) / post_list.length * 100)
-        
-
+	# check for signs of a yahoo error page, with causes minidom to flip out
+	if(len(content) >=6 and content[:5] == '<!-- ' or 
+		content[-4:] == " -->"):
+		print content
+		print "yahoo error, no data fetched "
+		return
+	
+	try:
+		x = minidom.parseString(content)
+	except expat.ExpatError, e:
+		print content
+		print "== Above content caused minidom to flipped out %s " % (e)
+		return
+		
+	# sample post: <post href="http://www.pixelbeat.org/cmdline.html" hash="e3ac1d1e4403d077ee7e65f62a55c406" description="Linux Commands - A practical reference" tag="linux tutorial reference" time="2010-11-29T01:07:35Z" extended="" meta="c79362665abb0303d577b6b9aa341599" />
+	
+	post_list = x.getElementsByTagName('post')
+	
+	for post_index, post in enumerate(post_list):
+	
+		try:
+			url = post.getAttribute('href')
+			desc = post.getAttribute('description')
+			# turn a comma separated list of tags into a real list of tags
+			tags = ",".join([t for t in post.getAttribute('tag').split()])
+			timestamp = post.getAttribute('time')		
+			options = {'tags': tags, 'push':False, 'msg':desc, "lazyAdd":True}
+			
+			# Set the authoring date of the commit based on the imported
+			# timestamp. git reads the GIT_AUTHOR_DATE environment var.
+			os.environ['GIT_AUTHOR_DATE'] = timestamp
+	
+			args = [url]
+			g = gitMark(options, args)
+			
+			# Reset authoring timestamp (abundance of caution)
+			del os.environ['GIT_AUTHOR_DATE']
+			
+			if post_list.length > 1:
+				print '%d of %d bookmarks imported (%d%%)' % (
+					post_index + 1, post_list.length,
+					(post_index + 1.0) / post_list.length * 100)
+		except (KeyboardInterrupt, SystemExit):
+			print >>sys.stderr, ("backup interrupted by KeyboardInterrupt/SystemExit" )
+			return 
+			
 if __name__ == '__main__':
-    try:
-        (username, password) = sys.argv[1:]
-    except ValueError:
-        print "Usage: python delicious_import.py username password"
-        
-    d = delicious_import(username, password)
+	try:
+		(username, password) = sys.argv[1:]
+	except ValueError:
+		print "Usage: python delicious_import.py username password"
+			
+	import_delicious_to_local_git(username, password)
 
--- gitmark.py
+++ gitmark.py
@@ -8,11 +8,12 @@ Copyright (c) 2010 Hilary Mason. All rights reserved.
 """
 
 import sys, os
-import urllib
+import urllib, httplib
 import re
 import hashlib
 import csv
 import subprocess
+import time
 from optparse import OptionParser
 
 from settings import *
@@ -24,94 +25,118 @@ from settings import *
 USE_SHELL = os.name == 'nt'
 
 class gitMark(object):
-    
-    def __init__(self, options, args):
-        modified = [] # track files we need to add - a hack, because it will add files that are already tracked by git
-        
-        url = args[0].strip('/')
-        content = self.getContent(url)
-        title = self.parseTitle(content)
-        content_filename = self.generateHash(url)
-        
-        modified.append(self.saveContent(content_filename, content))
-        tags = ['all']
-        tags.extend(options['tags'].split(','))
-        for tag in tags:
-            t = tag.strip()
-            if not t:
-                continue
-            if '/' in t:
-                t = ''.join(t.split('/'))
-            modified.append(self.saveTagData(t, url, title, content_filename))
-            
-        self.gitAdd(modified)
-        
-        commit_msg = options['msg']
-        if not commit_msg:
-            commit_msg = 'adding %s' % url
-        
-        self.gitCommit(commit_msg)
-        
-        if options['push']:
-            self.gitPush()
+	""" Object representing a bookmark stored as a git object """
+	def __init__(self, options, args):
 
-    def gitAdd(self, files):
-        subprocess.call(['git', 'add'] + files, shell=USE_SHELL)
-        
-    def gitCommit(self, msg):
-        subprocess.call(['git', 'commit', '-m', msg], shell=USE_SHELL)
-        
-    def gitPush(self):
-        pipe = subprocess.Popen("git push origin master", shell=True)
-        pipe.wait()
-        
-    def saveContent(self, filename, content):
-        f = open('%s%s' % (CONTENT_PATH, filename), 'w')
-        f.write(content)
-        f.close()
-        return '%s%s' % (CONTENT_PATH, filename)
-        
-    def saveTagData(self, tag, url, title, content_filename):
-        tag_writer = csv.writer(open('%s%s' % (TAG_PATH, tag), 'a'))
-        tag_writer.writerow([url, title, content_filename])
-        return '%s%s' % (TAG_PATH, tag)
+		modified = [] # track files we need to add - a hack, because it will add files that are already tracked by git
+		url = args[0].strip('/')
+		
+		content_filename = self.generateHash(url)
 
-    def parseTitle(self, content):
-        re_htmltitle = re.compile(".*<title>(.*)</title>.*")
-        t = re_htmltitle.search(content)
-        try:
-            title = t.group(1)
-        except AttributeError:
-            title = '[No Title]'
-        
-        return title
-        
-    def generateHash(self, text):
-        m = hashlib.md5()
-        m.update(text)
-        return m.hexdigest()
-        
-    def getContent(self, url):
-        try:
-            h = urllib.urlopen(url)
-            content = h.read()
-            h.close()
-        except IOError, e:
-            print >>sys.stderr, ("Error: could not retrieve the content of a"
-                " URL. The bookmark will be saved, but its content won't be"
-                " searchable. URL: <%s>. Error: %s" % (url, e))
-            content = ''
-        return content
-        
+		if ( os.path.isfile('/'.join(["content",content_filename]))	 and options['lazyAdd'] == True ):
+				print "file exists in repo, lazyAdd is true, skipping add"
+				return
+		print url
+		content = self.getContent(url)
+		title = self.parseTitle(content)
+		
+		modified.append(self.saveContent(content_filename, content))
+		tags = ['all']
+		tags.extend(options['tags'].split(','))
+		for tag in tags:
+			t = tag.strip()
+			if not t:
+				continue
+			if '/' in t:
+				t = ''.join(t.split('/'))
+			modified.append(self.saveTagData(t, url, title, content_filename))
+			
+		self.gitAdd(modified)
+		
+		commit_msg = options['msg']
+		if not commit_msg:
+			commit_msg = 'adding %s' % url
+		
+		self.gitCommit(commit_msg)
+		
+		if options['push']:
+			self.gitPush()
+
+
+	@classmethod
+	def gitAdd(cls, files):
+		""" add this git object's files to the local repository"""
+		subprocess.call(['git', 'add'] + files, shell=USE_SHELL)
+		
+	@classmethod
+	def gitCommit(cls, msg):
+		""" commit the local repository to the server"""
+		subprocess.call(['git', 'commit', '-m', msg], shell=USE_SHELL)
+		
+	@classmethod
+	def gitPush(cls):
+		""" push the local origin to the master"""
+		pipe = subprocess.Popen("git push origin master", shell=True)
+		pipe.wait()
+	
+	@classmethod
+	def saveContent(cls, filename, content):
+		f = open('%s%s' % (CONTENT_PATH, filename), 'w')
+		f.write(content)
+		f.close()
+		return '%s%s' % (CONTENT_PATH, filename)
+		
+	@classmethod
+	def saveTagData(cls, tag, url, title, content_filename):
+		tag_writer = csv.writer(open('%s%s' % (TAG_PATH, tag), 'a'))
+		tag_writer.writerow([url, title, content_filename])
+		return '%s%s' % (TAG_PATH, tag)
+
+	@classmethod
+	def parseTitle(cls, content):
+		re_htmltitle = re.compile(".*<title>(.*)</title>.*")
+		t = re_htmltitle.search(content)
+		try:
+			title = t.group(1)
+		except AttributeError:
+			title = '[No Title]'
+		
+		return title
+		
+	@classmethod
+	def generateHash(cls, text):
+		m = hashlib.md5()
+		m.update(text)
+		return m.hexdigest()
+		
+	@classmethod
+	def getContent(cls, url):
+		try:
+			h = urllib.urlopen(url)
+			content = h.read()
+			h.close()
+			h = urllib.urlopen(url)
+		except IOError, e:
+			print >>sys.stderr, ("Error: could not retrieve the content of a"
+				" URL. The bookmark will be saved, but its content won't be"
+				" searchable. URL: <%s>. Error: %s" % (url, e))
+			content = ''
+		except httplib.InvalidURL, e: #case: a redirect is giving me www.idealist.org:, which causes a fail during port-number search due to trailing :
+			print >>sys.stderr, ("Error: url or url redirect contained an"
+			"invalid  URL. The bookmark will be saved, but its content"
+			"won't be searchable. URL: <%s>. Error: %s" % (url, e))
+			content=''
+		return content
 
 
 if __name__ == '__main__':
-    parser = OptionParser("usage: %prog [options]")
-    parser.add_option("-p", "--push", dest="push", action="store_false", default=True, help="don't push to origin.")
-    parser.add_option("-t", "--tags", dest="tags", action="store", default='notag', help="comma seperated list of tags")
-    parser.add_option("-m", "--message", dest="msg", action="store", default=None, help="specify a commit message (default is 'adding [url]')")
-    (options, args) = parser.parse_args()
-    
-    opts = {'push': options.push, 'tags': options.tags, 'msg': options.msg}
-    
-    g = gitMark(opts, args)
+
+	parser = OptionParser("usage: %prog [options]")
+	parser.add_option("-p", "--push", dest="push", action="store_true", default=False, help="don't push to origin.")
+	parser.add_option("-t", "--tags", dest="tags", action="store", default='notag', help="comma seperated list of tags")
+	parser.add_option("-m", "--message", dest="msg", action="store", default=None, help="specify a commit message (default is 'adding [url]')")
+	(options, args) = parser.parse_args()
+	
+	opts = {'push': options.push, 'tags': options.tags, 'msg': options.msg}
+	
+	g = gitMark(opts, args)
